51365f721e949edb91cf71ef4b90c8c064bc4bc7
skandhwa <113922787+skandhwa@users.noreply.github.com>
2025-08-28 15:38:20 +0530

    Add files via upload

---
+++ MyExample1.java
@@ -0,0 +1,27 @@
+package ArrayExamples;
+
+public class MyExample1 {
+
+	public static void main(String[] args) {
+		
+		int []a= {12,34,67,88,99};
+		
+		int []b=new int[] {67,88,99,102};
+		
+		
+		for(int i=0;i<a.length;i++)
+			
+		{
+			System.out.println(a[i]);
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
